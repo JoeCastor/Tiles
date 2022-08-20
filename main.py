4abd851777d156361fe12261c9d133dd63da8801
JoeCastor <u7309735@anu.edu.au>
2022-08-20 22:38:22 +1000

    Optimisation

--- main.py
+++ main.py
@@ -34,63 +34,69 @@ def place_l(x1, y1, x2, y2, x3, y3):
     array[x2][y2] = tile_count
     array[x3][y3] = tile_count
 
+
 # Recursively places L-shaped tiles until the ceiling is covered
-def tile_recurse(n, row_offset, col_offset):
+def tile_recurse(n, row_offset, col_offset, fan_row, fan_col):
     global tile_count
-    row_of_fan = -1
-    col_of_fan = -1
 
     # Base case:
     if (n==2):
         tile_count+=1
-        for i in range(n):
-            for j in range(n):
-                if (array[row_offset+i][col_offset + j]==1):
-                    array[row_offset + i][col_offset + j] = tile_count
+        if (row_offset != fan_row or col_offset != fan_col):
+            array[row_offset][col_offset] = tile_count
+        if (row_offset != fan_row or col_offset + 1 != fan_col):
+            array[row_offset][col_offset + 1] = tile_count
+        if (row_offset + 1 != fan_row or col_offset != fan_col):
+            array[row_offset + 1][col_offset] = tile_count
+        if (row_offset + 1 != fan_row or col_offset + 1 != fan_col):
+            array[row_offset + 1][col_offset + 1] = tile_count
         return
-
-    for i in range(row_offset, row_offset + n):
-        for j in range(col_offset, col_offset + n):
-            if array[i][j]!=1:
-                row_of_fan = i
-                col_of_fan = j
 
     # placing the tile that has one square in each quadrant:
     upper_row = row_offset + int(n/2) -1
     lower_row = row_offset + int(n/2)
     right_col = col_offset + int(n/2)
     left_col = col_offset + int(n/2) - 1
-    # q0
-    if (row_of_fan < row_offset + n/2 and col_of_fan < col_offset + n/2):
+
+    # if fan is in q0:
+    if (fan_row < row_offset + n/2 and fan_col < col_offset + n/2):
         place_l(upper_row, right_col, lower_row, left_col, lower_row, right_col)
 
-    #q1
-    elif (row_of_fan < row_offset + n/2 and col_of_fan >= col_offset + n/2):
+        tile_recurse(int(n / 2), row_offset, col_offset, fan_row, fan_col)
+        tile_recurse(int(n / 2), row_offset, col_offset + int(n / 2), row_offset + int(n/2) - 1, col_offset + int(n / 2))
+        tile_recurse(int(n / 2), row_offset + int(n / 2), col_offset, row_offset + int(n/2), col_offset + int(n/2) -1)
+        tile_recurse(int(n / 2), row_offset + int(n / 2), col_offset + int(n / 2), row_offset + int(n/2), col_offset + int(n/2))
+
+    # if fan is in q1
+    elif (fan_row < row_offset + n / 2 and fan_col >= col_offset + n / 2):
         place_l(upper_row, left_col, lower_row, left_col, lower_row, right_col)
 
-    #q2
-    elif (row_of_fan >= row_offset + n/2 and col_of_fan < col_offset + n/2):
+        tile_recurse(int(n / 2), row_offset, col_offset, row_offset + int(n/2) -1, col_offset + int(n/2 - 1))
+        tile_recurse(int(n / 2), row_offset, col_offset + int(n / 2), fan_row, fan_col)
+        tile_recurse(int(n / 2), row_offset + int(n / 2), col_offset, row_offset + int(n / 2), col_offset + int(n / 2) - 1)
+        tile_recurse(int(n / 2), row_offset + int(n / 2), col_offset + int(n / 2), row_offset + int(n / 2), col_offset + int(n / 2))
+
+    # if fan is in q2
+    elif (fan_row >= row_offset + n / 2 and fan_col < col_offset + n / 2):
         place_l(upper_row, left_col, upper_row, right_col, lower_row, right_col)
 
-    #q3
+        tile_recurse(int(n / 2), row_offset, col_offset, row_offset + int(n / 2) - 1, col_offset + int(n / 2 - 1))
+        tile_recurse(int(n / 2), row_offset, col_offset + int(n / 2), row_offset + int(n / 2) - 1, col_offset + int(n / 2))
+        tile_recurse(int(n / 2), row_offset + int(n / 2), col_offset, fan_row, fan_col)
+        tile_recurse(int(n / 2), row_offset + int(n / 2), col_offset + int(n / 2), row_offset + int(n / 2), col_offset + int(n / 2))
+
+    # if fan is in q3
     else:
         place_l(upper_row, left_col, upper_row, right_col, lower_row, left_col)
 
-    #Recursive calls tile on each quadrant:
-    tile_recurse(int(n / 2), row_offset, col_offset)
-    tile_recurse(int(n / 2), row_offset, col_offset + int(n / 2))
-    tile_recurse(int(n / 2), row_offset + int(n / 2), col_offset)
-    tile_recurse(int(n / 2), row_offset + int(n / 2), col_offset + int(n / 2))
-
+        tile_recurse(int(n / 2), row_offset, col_offset, row_offset + int(n / 2) - 1, col_offset + int(n / 2 - 1))
+        tile_recurse(int(n / 2), row_offset, col_offset + int(n / 2), row_offset + int(n / 2) - 1, col_offset + int(n / 2))
+        tile_recurse(int(n / 2), row_offset + int(n / 2), col_offset, row_offset + int(n / 2), col_offset + int(n / 2) - 1)
+        tile_recurse(int(n / 2), row_offset + int(n / 2), col_offset + int(n / 2), fan_row, fan_col)
     return
 
-# Since we check for 1s in the tile function, we skip tile 1 and go straight to tile 2
-# Therefore, we replace the highest numbered tile with 1 at the end.
-def  fix_first():
-    for i in range(len(array)):
-        for j in range(len(array)):
-            if array[i][j]==tile_count:
-                array[i][j] = 1
+
+
 
 # main
 if __name__ == '__main__':
@@ -98,9 +104,9 @@ if __name__ == '__main__':
     index = int(input())
     array = generate_input(size, index)
 
-    tile_count = 1
-    tile_recurse(len(array), 0, 0)
-    fix_first()
+    tile_count = 0
+    tile_recurse(len(array), 0, 0, int(index / size), index % size)
+
     print("Output:")
     print_array(array)
 
